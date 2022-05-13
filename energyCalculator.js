a072b3cd2a912201ab4e6e97b92c0e81230bd181
Juuso Soikkeli <juuso.soikkeli@cavai.com>
2022-05-13 14:59:09 +0300

    added energy estimate of asset loading

--- energyCalculator.js
+++ energyCalculator.js
@@ -1,3 +1,5 @@
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
 const renderText = text => {
   const eventLog = document.querySelector('#event-log')
 
@@ -7,7 +9,18 @@ const renderText = text => {
   eventLog.appendChild(span)
 }
 
-const calculateEnergyConsumption = navigationEntry => {
+const calculateLoadingEnergyConsumption = resourceEntry => {
+  const loadingDuration = resourceEntry.duration
+
+  const watts = 0.1 * 10 // hard-coded 10 assets per creative
+  const renderTimeInHours = (loadingDuration / (1000 * 60 * 60))
+  const wh = watts * renderTimeInHours
+
+  renderText(`Ad loaded in ${loadingDuration.toFixed(2)}ms`)
+  renderText(`Energy consumption from loading ~${wh.toString().substring(0, 10)}Wh`)
+}
+
+const calculateRenderingEnergyConsumption = navigationEntry => {
   const renderDuration = navigationEntry.duration
 
   const watts = !isMobile ? 100 : 5
@@ -15,16 +28,22 @@ const calculateEnergyConsumption = navigationEntry => {
   const wh = watts * renderTimeInHours
 
   renderText(`Ad rendered in ${renderDuration.toFixed(2)}ms`)
-  renderText(`Energy consumption approximately ${wh.toString().substring(0, 7)}Wh`)
+  renderText(`Energy consumption from rendering ~${wh.toString().substring(0, 7)}Wh`)
 }
-
-const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 
 const observer = new PerformanceObserver((list) => {
   const entries = list.getEntries()
+  const resourceEntry = entries.find(({ entryType, initiatorType }) =>
+      entryType === 'resource' && initiatorType === 'script')
   const navigationEntry = entries.find(({ entryType }) => entryType === 'navigation')
+
+  if (resourceEntry) {
+    calculateLoadingEnergyConsumption(resourceEntry)
+    return
+  }
+
   if (navigationEntry) {
-    calculateEnergyConsumption(navigationEntry)
+    calculateRenderingEnergyConsumption(navigationEntry)
   }
 });
 
